a5af8140a589486a0e28669d315e4c1ae5dcc264
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2019-05-22 14:30:29 -0400

    Adding remaining HTML, CSS and JavaScript files

---
+++ behavior.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail");
+    thumbnailElement.addEventListener("click", function() {
+
+	if (thumbnailElement.className == "") {
+	    thumbnailElement.className = "small";
+	} else {
+	    thumbnailElement.className = "";
+	}
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail1");
+    thumbnailElement.addEventListener("click", function() {
+
+	if (thumbnailElement.className == "") {
+	    thumbnailElement.className = "small";
+	} else {
+	    thumbnailElement.className = "";
+	}
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail2");
+    thumbnailElement.addEventListener("click", function() {
+
+	if (thumbnailElement.className == "") {
+	    thumbnailElement.className = "small";
+	} else {
+	    thumbnailElement.className = "";
+	}
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var thumbnailElement = document.getElementById("smart_thumbnail3");
+    thumbnailElement.addEventListener("click", function() {
+
+	if (thumbnailElement.className == "") {
+	    thumbnailElement.className = "small";
+	} else {
+	    thumbnailElement.className = "";
+	}
+    });
+});
